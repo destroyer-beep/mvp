d77bef4b0c7c6e9848a0cdeee9efcba4376d5cfd
vhramov <vhramov@luxms.com>
2023-08-25 18:39:16 +0300

    create components

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  params: {
+    id: string
+  }
+}
+
+export function Profile({params: { id }}) {
+  return <div>
+    Profile
+  </div>
+}
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,5 @@
+import styles from './Card.module.scss'
+
+export function Card() {
+  return <div className={styles.card}>Card</div>
+}
