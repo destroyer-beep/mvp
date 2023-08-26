b259bda3ab9d6c3ac90a54b391f5ff6cab0d0436
Vladimir <destroyer.hramov@yandex.ru>
2023-08-26 19:15:39 +0300

    create cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,26 @@
 import styles from './Card.module.scss'
+import Image from "next/image";
 
-export default function Card() {
-  return <div className={styles.card}>Card</div>
+type TProduct = {
+  name: string,
+  title: string,
+  price: number,
+  status: string,
+  owner: string,
+  grade: number
+}
+
+type TData = {
+  data: TProduct,
+  img: any
+}
+export default function Card({data, img}:TData) {
+  return <div className={styles.card}>
+    <div className={styles.card__img}>
+      <Image src={img} width={260} alt={'Product'} priority/>
+    </div>
+    <div className={styles.card__name}>{data.name}</div>
+    <div className={styles.card__title}>{data.title}</div>
+    <div className={styles.card__price}>от {data.price.toLocaleString('ru')} ₽</div>
+  </div>
 }
